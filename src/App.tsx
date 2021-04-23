fac5a66e78930ad7cb5ea62c2b62c55f57f1e79d
KoseiAoyagi <45550286+azul915@users.noreply.github.com>
2021-04-24 02:31:59 +0900

    fix apperarance of chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,67 @@
 //import "./styles.css";
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 
 function App() {
   const data = [
-    { name: "2010年", 株価: 4000, 配当: 2400 },
-    { name: "2011年", 株価: 3000, 配当: 1398 },
-    { name: "2012年", 株価: 2000, 配当: 9800 },
-    { name: "2013年", 株価: 2780, 配当: 3908 },
-    { name: "2014年", 株価: 1890, 配当: 4800 },
-    { name: "2015年", 株価: 2390, 配当: 3800 },
-    { name: "2016年", 株価: 3490, 配当: 4300 },
-    { name: "2017年", 株価: 5700, 配当: 6300 },
-    { name: "2018年", 株価: 5490, 配当: 3200 }
+    { name: "04/17", lgtm: 76 },
+    { name: "04/18", lgtm: 77 },
+    { name: "04/19", lgtm: 72 },
+    { name: "04/20", lgtm: 75 },
+    { name: "04/21", lgtm: 68 },
+    { name: "04/22", lgtm: 73 },
+    { name: "04/23", lgtm: 78 },
+    { name: "04/24", lgtm: 83 },
+    { name: "04/25", lgtm: 84 }
   ];
   return (
     <div className="App">
-      <LineChart width={700} height={500} data={data}>
+      <LineChart
+        width={700}
+        height={500}
+        data={data}
+        margin={{
+          top: 50,
+          right: 20,
+          left: 50,
+          bottom: 20
+        }}
+      >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" interval="preserveStartEnd" />
-        <YAxis interval="preserveStartEnd" />
+        <XAxis
+          dataKey="name"
+          domain={["dataMin", "dataMax"]}
+          interval="preserveStartEnd"
+          label={{
+            value: "日付",
+            offset: -10,
+            position: "insideBottomRight"
+          }}
+        />
+        <YAxis
+          interval="preserveStartEnd"
+          label={{
+            value: "LGTM",
+            angle: 90,
+            position: "insideLeft"
+          }}
+        />
         <Legend />
         <Line
           type="monotone"
-          dataKey="株価"
+          dataKey="lgtm"
           stroke="#8884d8"
-          activeDot={{ r: 8 }}
+          fill="#8884d8"
+          strokeWidth={2}
         />
-        <Line type="monotone" dataKey="配当" stroke="#82ca9d" />
       </LineChart>
     </div>
   );
